9fc724edd29dd19f4d8a5867c6290c547622d24a
Dragos-Ronald Rugescu <drugescu@luxoft.com>
2018-09-27 13:46:31 +0300

    sort (MergeSort), flush

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
  *
  */
 
+#define _XOPEN_SOURCE 600
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -35,7 +37,7 @@ pthread_barrier_t threads_initialized;
 list_node_t *head = NULL;
 
 void pf(list_node_t *item) {
-    printf("(val : %d)", item->val);
+    printf("%d", item->val);
 }
 
 // Adds a new element to the end of the list
@@ -135,6 +137,67 @@ void delete_element(int val, int tid) {
     pthread_mutex_unlock(&list_mutex);
 }
 
+// Count in O(n)
+int _count_elements(list_node_t *head) {
+    int count = 0;
+    list_node_t *current = head;
+
+    // Iterate all through the list
+    while (current->next != NULL) {
+        current = current->next;
+        count++;
+    }
+
+    return count;
+}
+
+// Modifies <<first>> and <<second>> by splitting <<lst>> equally
+void _split_list(list_node_t *lst, list_node_t **first, list_node_t **second) {
+    list_node_t *fast = NULL;
+    list_node_t *slow = NULL;
+
+    // Split in O(n) instead of O(2n) by counting twice (fast-slow ptr)
+    slow = lst;
+    fast = lst->next;
+
+    // Fast advances two nodes, slow advances one
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    // Slow is before midpoint, split here
+    *first = lst;
+    *second = slow->next;
+    slow->next = NULL;
+}
+
+list_node_t *_sorted_merge(list_node_t *first, list_node_t *second) {
+    // Point end of first to beginning of second
+    list_node_t *result = NULL;
+
+    // Simplest cases
+    if (first == NULL)
+        return second;
+    else if (second == NULL)
+        return first;
+
+    // Pick either, or recurse
+    if (first->val <= second->val) {
+        result = first;
+        result->next = _sorted_merge(first->next, second);
+    }
+    else {
+        result = second;
+        result->next = _sorted_merge(first, second->next);
+    }
+
+    return result;
+}
+
 // Prints the list using the callback function of each element
 void print_list(list_node_t *head, int tid) {
 
@@ -157,6 +220,81 @@ void print_list(list_node_t *head, int tid) {
     pthread_mutex_unlock(&list_mutex);
 }
 
+// Quick prints a list without locking it (assumes a lock was already set!)
+void _print_list_nolock(list_node_t *head, int tid) {
+    list_node_t *current = head;
+
+    printf("       [%d] List ( ", tid);
+
+    while (current != NULL) {
+        current->callback(current);
+        printf(" ");
+        current = current->next;
+    }
+
+    printf(")\n");
+}
+
+void _merge_sort(list_node_t **headRef, int tid) {
+    printf ("[Thread %d] Started MergeSort\n", tid);
+
+    list_node_t *head = *headRef;
+
+    // Don't sort empty list or one element list
+    int isEmpty = (head == NULL);
+    int isLengthOne = (head->next == NULL);
+    if (isEmpty || isLengthOne) {
+        // Don't forget about the lock
+        printf ("[Thread %d] List empty!\n", tid);
+        pthread_mutex_unlock(&list_mutex);
+        return;
+    }
+
+    // Divide list in two halves
+    list_node_t *first = NULL;
+    list_node_t *second = NULL;
+
+    _split_list(head, &first, &second);
+    printf ("[Thread %d] Split list (first, second):\n", tid);
+    _print_list_nolock(first, tid);
+    _print_list_nolock(second, tid);
+
+    // Sort the two halves
+    _merge_sort(&first, tid);
+    _merge_sort(&second, tid);
+
+    // Merge the sorted halves
+    *headRef = _sorted_merge(first, second);
+    printf ("[Thread %d] Merged ordered sublists:\n", tid);
+    _print_list_nolock(head, tid);
+}
+
+// Merge sort list by pointer rearrangement and slow/fast pointer split midway
+void sort_list(list_node_t **headRef, int tid) {
+    // Lock the list
+    pthread_mutex_lock(&list_mutex);
+
+    _merge_sort(headRef, tid);
+
+    // Release the list
+    pthread_mutex_unlock(&list_mutex);
+}
+
+// Deletes all elements in list
+void flush_list(list_node_t **headRef) {
+    list_node_t *current = *headRef;
+    list_node_t *next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    // Cleanup real head
+    *headRef = NULL;
+}
+
 // Per thread operations on linked list
 void *work_function(void *thread_id) {
     int tid;
@@ -164,36 +302,57 @@ void *work_function(void *thread_id) {
     tid = *((int*) thread_id);
 
     // Ambiguity in call result for first successful call of pthread_create
-    if (tid > NUM_THREADS)
+    int isHighTID = (tid > NUM_THREADS);
+    if (isHighTID)
         tid = 0;
 
     // Wait for initialization of all threads
     pthread_barrier_wait(&threads_initialized);
+    if (tid == 0)
+        printf ("|Thread %d| Master barrier reached, syncronized threads!\n", getpid());
 
     printf ("[Thread %d] Hello world!\n", tid);
 
-    // Operate on list
+    // Operate on list - insert sleeps for round-robin thread scheduler
     if (tid == 1) {
         add_element(11, tid);
+        usleep(1);
         add_element(1, tid);
+        usleep(1);
         delete_element(11, tid);
+        usleep(1);
         add_element(8, tid);
+        usleep(1);
         print_list(head, tid);
+        usleep(1);
     }
     else if (tid == 2) {
         add_element(30, tid);
+        usleep(1);
         add_element(25, tid);
+        usleep(1);
         add_element(100, tid);
+        usleep(1);
+        sort_list(&head, tid);
+        usleep(1);
         delete_element(100, tid);
-        print_list(head, tid);
+        usleep(1);
     }
     else {
         add_element(2, tid);
+        usleep(1);
         add_element(4, tid);
+        usleep(1);
         add_element(10, tid);
+        usleep(1);
         delete_element(2, tid);
+        usleep(1);
+        sort_list(&head, tid);
+        usleep(1);
         delete_element(10, tid);
+        usleep(1);
         delete_element(5, tid);
+        usleep(1);
     }
 
     return NULL;
@@ -231,6 +390,12 @@ int cleanup()
     }
 
     printf("All threads completed successfully!\n");
+
+    print_list(head, getpid());
+
+    printf("Now flushing the list.\n");
+
+    flush_list(&head);
 
     print_list(head, getpid());
 
